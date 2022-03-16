7f3af8e490a4532d235e0bd1e8c8ac6814536af1
Snehdeep Singh <essdees27@gmail.com>
2022-03-16 14:48:03 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
